90b17ac8d8cf721d967d54270b64f2631a52deba
sarvi <pr486824@dal.ca>
2020-10-23 19:51:03 +0530

    Changes in main removing the mock object

--- src/main/java/dal/asd/dpl/App.java
+++ src/main/java/dal/asd/dpl/App.java
@@ -1,8 +1,6 @@
 package dal.asd.dpl;
 
 import dal.asd.dpl.Database.LeagueDataDB;
-import dal.asd.dpl.NewsSystem.IGamesPlayed;
-import dal.asd.dpl.NewsSystem.NewsSystem;
 import dal.asd.dpl.SimulationStateMachine.*;
 import dal.asd.dpl.TeamManagement.ILeague;
 import dal.asd.dpl.UserInput.CmdUserInput;
