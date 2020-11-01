67950a657fdc952c5b872647dbfb624fe555ca6f
sarvi <pr486824@dal.ca>
2020-11-01 10:39:21 +0530

    Testing Notify method

--- src/test/java/dal/asd/dpl/NewsSystem/FreeAgencyTest.java
+++ src/test/java/dal/asd/dpl/NewsSystem/FreeAgencyTest.java
@@ -11,31 +11,30 @@ import org.junit.Test;
 
 public class FreeAgencyTest {
 	private final ByteArrayOutputStream console = new ByteArrayOutputStream();
-	//private IFreeAgencyInfo subscriber = new NewsSubscriber();
+	private IFreeAgencyInfo subscriber = new NewsSubscriber();
 
 	
 	@Before
 	public void before() {
 		System.setOut(new PrintStream(console));
-		//FreeAgencyPublisher.getInstance().getSubscribers().clear();
-		//FreeAgencyPublisher.getInstance().subscribe(subscriber);
+		FreeAgencyPublisher.getInstance().subscribe(subscriber);
 	}
 	
 	@After
 	public void after() {
 		System.setOut(null);
-		//FreeAgencyPublisher.getInstance().unsubscribe(subscriber);
+		FreeAgencyPublisher.getInstance().unsubscribe(subscriber);
 	}
 	
 	@Test
 	public void outputJsonHiredTest() {
 		FreeAgencyPublisher.getInstance().notify("Wayne Gretzky", "hired");
-		//assertEquals(OutputConstants.HIRED, console.toString().trim());
+		assertEquals(OutputConstants.HIRED, console.toString().trim());
 	}
 	
 	@Test
 	public void outputJsonReleasedTest() {
 		FreeAgencyPublisher.getInstance().notify("Wayne Gretzky", "released");
-		//assertEquals(OutputConstants.RELEASED, console.toString().trim());
+		assertEquals(OutputConstants.RELEASED, console.toString().trim());
 	}
 }
