f60fa1fdf5e620d3565d83a57f6a0e6438a2bb38
sarvi <pr486824@dal.ca>
2020-11-03 07:34:00 +0530

    Code Clearence

--- src/test/java/dpl/NewsSystemTest/GamePlayedTest.java
+++ src/test/java/dpl/NewsSystemTest/GamePlayedTest.java
@@ -1,10 +1,15 @@
 package dpl.NewsSystemTest;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import dpl.NewsSystem.GamePlayedPublisher;
 
 public class GamePlayedTest {
 	private final ByteArrayOutputStream console = new ByteArrayOutputStream();
@@ -19,4 +24,9 @@ public class GamePlayedTest {
 		System.setOut(null);
 	}
 
+	@Test
+	public void outputJsonTest() {
+		GamePlayedPublisher.getInstance().notify("Montreal Canadiens", "Toronto Maple Leafs", "25-10-2020");
+		assertEquals(OutputConstants.GAMEPLAY, console.toString().trim());
+	}
 }
--- src/test/java/dpl/NewsSystemTest/RetirementTest.java
+++ src/test/java/dpl/NewsSystemTest/RetirementTest.java
@@ -1,10 +1,15 @@
 package dpl.NewsSystemTest;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import dpl.NewsSystem.RetirementPublisher;
 
 public class RetirementTest {
 	private final ByteArrayOutputStream console = new ByteArrayOutputStream();
@@ -19,4 +24,9 @@ public class RetirementTest {
 		System.setOut(null);
 	}
 
+	@Test
+	public void outputJsonTest() {
+		RetirementPublisher.getInstance().notify("Wayne Gretzky", 38);
+		assertEquals(OutputConstants.RETIRED, console.toString().trim());
+	}
 }
