4a76a83d94fd5fa193e4a4ce5e9e254c705bb7c5
sarvi <pr486824@dal.ca>
2020-11-28 05:07:55 +0530

    Trophy System Abstract Factory & tests

---
+++ src/main/java/dpl/LeagueSimulationManagement/TrophySystem/AwardedTrophies.java
@@ -0,0 +1,50 @@
+package dpl.LeagueSimulationManagement.TrophySystem;
+
+import dpl.LeagueSimulationManagement.LeagueManagement.TeamManagement.Coach;
+import dpl.LeagueSimulationManagement.LeagueManagement.TeamManagement.Player;
+
+public class AwardedTrophies {
+
+    public void bestTeam(int year){
+        String bestTeam = TeamPoints.getInstance().getBestTeam();
+        TrophyHistory.getInstance().addTrophy(year, "President's Trophy", bestTeam);
+        System.out.println("Best Team :" + bestTeam);
+    }
+
+    public void bestPlayer(int year){
+        Player player = PlayerGoalScorer.getInstance().getBestPlayer();
+        TrophyHistory.getInstance().addTrophy(year, "Calder Memorial Trophy", player.getPlayerName());
+        System.out.println("Best Player :" + player.getPlayerName());
+    }
+
+    public void bestGoalie(int year){
+        Player player = GoalSaver.getInstance().getBestGoalSaver();
+        TrophyHistory.getInstance().addTrophy(year, "Vezina Trophy", player.getPlayerName());
+        System.out.println("Best Goalie :" + player.getPlayerName());
+    }
+
+    public void bestCoach(int year){
+        Coach coach = BestCoachLeague.getInstance().getBestCoach();
+        TrophyHistory.getInstance().addTrophy(year, "Jack Adam's Award", coach.getCoachName());
+        System.out.println("Best Coach :" + coach.getCoachName());
+    }
+
+    public void bestScorer(int year){
+        Player player = TopGoalScorer.getInstance().getTopGoalScorer();
+        TrophyHistory.getInstance().addTrophy(year, "Maurice Richard Trophy", player.getPlayerName());
+        System.out.println("Best Scorer :" + player.getPlayerName());
+    }
+
+    public void bestDefencemen(int year){
+        Player player = BestDefenceMen.getInstance().getBestDefenceMen();
+        TrophyHistory.getInstance().addTrophy(year, "Rob Hawkey Memorial Cup", player.getPlayerName());
+        System.out.println("Best Defencemen :" + player.getPlayerName());
+    }
+
+    public void participationTeam(int year){
+        String participatedTeam = ParticipantsAward.getInstance().getTeamWithLowestPoints();
+        TrophyHistory.getInstance().addTrophy(year, "Participation Award", participatedTeam);
+        System.out.println("Participation Team :" + participatedTeam);
+    }
+
+}
---
+++ src/test/java/dpl/TrophySystemTest/TeamPointsTest.java
@@ -0,0 +1,45 @@
+package dpl.TrophySystemTest;
+
+import dpl.LeagueSimulationManagement.LeagueManagement.TeamManagement.Team;
+import dpl.LeagueSimulationManagement.NewsSystem.GamePlayedPublisher;
+import dpl.LeagueSimulationManagement.TrophySystem.IObserver;
+import dpl.LeagueSimulationManagement.TrophySystem.TeamPoints;
+import dpl.LeagueSimulationManagement.TrophySystem.TeamPointsObserver;
+import dpl.LeagueSimulationManagement.TrophySystem.TrophySystemAbstractFactory;
+import dpl.NewsSystemTest.OutputConstants;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class TeamPointsTest {
+    private IObserver observer;
+    private Team team;
+    @Before
+    public void before() {
+        observer = TrophySystemAbstractFactory.createObserver(TrophySystemAbstractFactory.PRESIDENTS_TROPHY);
+        TeamPoints.getInstance().attach(observer);
+        team = new Team();
+        team.setTeamName("TestTeam");
+    }
+
+    @After
+    public void after() {
+        TeamPoints.getInstance().detach(observer);
+    }
+
+    @Test
+    public void setAndGetValueTest() {
+        TeamPoints.getInstance().setValue("TestKey", "TestValue");
+        assertEquals("TestValue", TeamPoints.getInstance().getValue("TestKey"));
+    }
+
+    @Test
+    public void notifyAllObserversTest() {
+        TeamPoints.getInstance().notifyWhenATeamWinsTheMatch(team);
+        assertEquals("TestTeam", TeamPoints.getInstance().getBestTeam());
+    }
+}
